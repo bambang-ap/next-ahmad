0fd46e67d4c67252bd8e43a72d19a6c3001b2fe7
bambang.ap <bambang.ap@flash-coffee.com>
2023-01-06 18:36:10 +0700

    eslint config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,21 @@
-import "./globals.css";
+import './globals.css';
 
-import React from "react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { AppProps } from "next/app";
-import { RecoilRoot } from "recoil";
+import React from 'react';
+
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import {AppProps} from 'next/app';
+import {RecoilRoot} from 'recoil';
 
 const queryClient = new QueryClient();
 
-const App = ({ Component, pageProps }: AppProps) => {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <RecoilRoot>
-        <Component {...pageProps} />
-      </RecoilRoot>
-    </QueryClientProvider>
-  );
+const App = ({Component, pageProps}: AppProps) => {
+	return (
+		<QueryClientProvider client={queryClient}>
+			<RecoilRoot>
+				<Component {...pageProps} />
+			</RecoilRoot>
+		</QueryClientProvider>
+	);
 };
 
 export default App;
