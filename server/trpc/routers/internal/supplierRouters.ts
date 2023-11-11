767ee1cb0e9331a54d6cc61666e892979362f855
bambang-ap <adhyt.scott@gmail.com>
2023-11-11 23:11:29 +0700

    supplier search

--- server/trpc/routers/internal/supplierRouters.ts
+++ server/trpc/routers/internal/supplierRouters.ts
@@ -1,14 +1,25 @@
-import {sSupplier, tableFormValue, zId} from '@appTypes/app.zod';
+import {Op} from 'sequelize';
+
+import {SSupplier, sSupplier, tableFormValue, zId} from '@appTypes/app.zod';
 import {Success} from '@constants';
-import {oSup} from '@database';
+import {oSup, wherePagesV3} from '@database';
 import {checkCredentialV2, generateId, pagingResult} from '@server';
 import {procedure, router} from '@trpc';
 
 export const supplierRouters = router({
 	get: procedure.input(tableFormValue).query(({ctx, input}) => {
-		const {limit, page} = input;
+		const {limit, page, search} = input;
 		return checkCredentialV2(ctx, async () => {
-			const {count, rows} = await oSup.findAndCountAll();
+			const searcher = {[Op.iLike]: `%${search}%`};
+
+			const where = !search
+				? undefined
+				: wherePagesV3<SSupplier>(
+						{nama: searcher, alamat: searcher, telp: searcher},
+						'or',
+				  );
+
+			const {count, rows} = await oSup.findAndCountAll({where});
 
 			return pagingResult(
 				count,
