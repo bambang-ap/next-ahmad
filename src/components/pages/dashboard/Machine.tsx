c54613ba04c122fb8fdacde7948e0509366d094e
bambang-ap <adhyt.scott@gmail.com>
2023-11-27 23:06:18 +0700

    mobile view dashboard machine

--- src/components/pages/dashboard/Machine.tsx
+++ src/components/pages/dashboard/Machine.tsx
@@ -1,11 +1,16 @@
+import {useRecoilValue} from 'recoil';
+
 import {Gallery} from '@baseComps/Gallery';
 import {useTickerText} from '@hooks';
+import {atomIsMobile} from '@recoil/atoms';
 import {qtyMap} from '@utils';
 import {trpc} from '@utils/trpc';
 
 export default function MachineDashboard() {
 	const {data, isFetching} = trpc.dashboard.machine.list.useQuery();
 	const {isLoadingText} = useTickerText(isFetching);
+
+	const isMobile = useRecoilValue(atomIsMobile);
 
 	const machineData = entries(data);
 
@@ -18,7 +23,7 @@ export default function MachineDashboard() {
 	return (
 		<>
 			<Gallery
-				columns={5}
+				columns={isMobile ? 1 : 5}
 				data={machineData}
 				renderItem={({item: [, item]}) => {
 					const {nomor_mesin, dKatMesin} = item.mesin ?? {};
@@ -27,6 +32,10 @@ export default function MachineDashboard() {
 						<div className="flex flex-col border-2 border-black">
 							<div className="px-4 py-2 text-center flex-1 font-bold text-md">
 								{dKatMesin?.name} - {nomor_mesin}
+							</div>
+							<div className="flex justify-between px-4 py-2">
+								<div className="text-xl font-bold">Planning</div>
+								<div className="text-xl font-bold">Produksi</div>
 							</div>
 							<div className="border border-black" />
 							{qtyMap(({qtyKey, unitKey}) => {
@@ -39,10 +48,10 @@ export default function MachineDashboard() {
 								return (
 									<div className="flex flex-1 justify-center py-2">
 										<div className="w-full px-4">
-											<div className="text-left font-bold text-2xl">
+											<div className="text-left font-bold text-xl">
 												{qtyPlanning?.toFixed(2)} {unit[unitKey]}
 											</div>
-											<div className="text-right font-bold text-2xl">
+											<div className="text-right font-bold text-xl">
 												{qtyProduksi?.toFixed(2)} {unit[unitKey]}
 											</div>
 										</div>
