6f47b0d5b44b2b749770d71ad2efb73aa602c957
bambang-ap <adhyt.scott@gmail.com>
2024-01-19 02:34:52 +0700

    internal po item discount

--- src/components/app/DiscountSelection.tsx
+++ src/components/app/DiscountSelection.tsx
@@ -1,21 +1,23 @@
 import objectPath from 'object-path';
-import {Path, useWatch} from 'react-hook-form';
+import {FieldPath, useWatch} from 'react-hook-form';
 
 import {FormProps} from '@appTypes/app.type';
 import {Input} from '@baseComps/Input';
 import {ButtonGroup} from '@baseComps/Input/ButtonGroup';
 import {SelectPropsData} from '@baseComps/Input/Select';
+import {Button} from '@baseComps/Touchable/Button';
 import {DiscType} from '@enum';
 
 const btnData: SelectPropsData<DiscType>[] = [
-	{value: DiscType.Percentage, label: 'Persentase'},
-	{value: DiscType.Value, label: 'Nilai'},
+	{value: DiscType.Percentage},
+	{value: DiscType.Value, label: '123'},
 ];
 
-type Props<T extends {}> = FormProps<T> & Record<'type' | 'discount', Path<T>>;
+type Props<T extends {}> = FormProps<T, 'control' | 'resetField'> &
+	Record<'type' | 'discount', FieldPath<T>> & {vertical?: boolean};
 
 export function DiscountSelection<T extends {}>(props: Props<T>) {
-	const {control, type, discount} = props;
+	const {control, resetField, type, discount, vertical} = props;
 
 	const dataForm = useWatch({control});
 
@@ -24,21 +26,43 @@ export function DiscountSelection<T extends {}>(props: Props<T>) {
 	const discountEnabled = !!typeValue;
 	const isPercentage = typeValue === DiscType.Percentage;
 
+	const clearButton = (
+		<Button
+			icon="faClose"
+			onClick={() => {
+				resetField(type);
+				resetField(discount);
+			}}
+		/>
+	);
+
 	return (
 		<>
 			<ButtonGroup
-				clearable
+				className="w-full"
 				label="Diskon"
-				control={control}
-				fieldName={type}
 				data={btnData}
+				fieldName={type}
+				control={control}
+				vertical={vertical}
 			/>
+
 			{discountEnabled && (
 				<Input
+					label="Diskon"
 					type="decimal"
 					control={control}
 					fieldName={discount}
-					rightAcc={isPercentage ? <div>%</div> : undefined}
+					rightAcc={
+						isPercentage ? (
+							<>
+								<div className="mr-2">%</div>
+								{clearButton}
+							</>
+						) : (
+							clearButton
+						)
+					}
 					rules={
 						!isPercentage
 							? undefined
