6f06a9d0a83e52719d994678b5872f31455162f8
bambang-ap <adhyt.scott@gmail.com>
2024-01-29 02:28:26 +0700

    dashboard sales get nilai

--- pages/app/dashboard/transaksi.tsx
+++
@@ -1,8 +0,0 @@
-import {getLayout} from '@hoc';
-import DashboardTransaksi from '@pageComponent/dashboard/Transaksi';
-
-Index.getLayout = getLayout;
-
-export default function Index() {
-	return <DashboardTransaksi />;
-}
